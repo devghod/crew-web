c4f92a4d9d721a835a6fcd758e6f60559c1a3f34
devghod <gbtm.workspace@gmail.com>
2024-06-22 18:01:51 +0800

    Update: root page

--- frontend/src/_Router.tsx
+++ frontend/src/_Router.tsx
@@ -13,14 +13,11 @@ import
   } 
 from "react-router-dom";
 
-const Root = lazy(() => import('./routes/root'));
-const Contact = lazy(() => import('./routes/contact'));
-
 const ErrorPage = lazy(() => import('./error-page'));
+const RootPage = lazy(() => import('./pages/root'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
 const StatisticsPage = lazy(() => import('./pages/StatisticsPage'));
 const DebtCentralPage = lazy(() => import('./pages/debt'));
-
 const Dashboard = lazy(() => import('./components/Dashboard'));
 
 const Routers: React.FC = () => {
@@ -30,8 +27,7 @@ const Routers: React.FC = () => {
       <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path="/" element={<Root />} />
-            <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<RootPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/dashboard" element={<Dashboard />}>
               <Route index element={<StatisticsPage />} />
--- frontend/src/pages/root/RootPage.tsx
+++ frontend/src/pages/root/RootPage.tsx
@@ -1,9 +1,14 @@
-import { Outlet, Link } from "react-router-dom";
+import React from "react";
 
-export default function Root() {
+export type RootPage = {};
+
+const RootPage: React.FC<RootPage> = (props) => {
+  
   return (
     <div className="w-full h-screen text-center content-center">
       <div className="">Hey, this is an awesome website to <span className="hover:text-sky-500">discover</span>.</div>
     </div>
-  );
-}
+  )
+};
+
+export default RootPage;
