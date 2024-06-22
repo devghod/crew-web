861abeb3ff65d5a516602d3c151e4e37193ae2e8
devghod <gbtm.workspace@gmail.com>
2024-06-22 14:48:54 +0800

    Update: debtstats

---
+++ frontend/src/components/Debt/DebtStats.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export type DebtStats = {
+  debtStats: object;
+};
+
+const DebtStats: React.FC<DebtStats> = (props) => {
+
+  const { debtStats } = props;
+  
+  return (
+    <div className="grid grid-cols-3 text-center text-sm text-slate-500">
+      <div className="text-sky-500">
+        Total Debts:
+        <span className="ml-1 font-semibold">
+          {debtStats.total}
+        </span>
+      </div>
+      <div className="text-lime-500">
+        Paid:
+        <span className="ml-1 font-semibold">
+          {debtStats.paid}
+        </span>
+      </div>
+      <div className="text-rose-500">
+        Unpaid:
+        <span className="ml-1 font-semibold">
+          {debtStats.unpaid}
+        </span>
+      </div>
+    </div>
+  )
+};
+
+export default DebtStats;
--- frontend/src/features/Debt/DebtController.tsx
+++ frontend/src/features/Debt/DebtController.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DebtStats from '../../components/Debt/DebtStats';
 
 export type DebtController = {
   open: () => void;
@@ -48,28 +49,7 @@ const DebtController: React.FC<DebtController> = (props) => {
               </div>
             </div>
           )}
-          {!isLoading && (
-            <div className="grid grid-cols-3 text-center text-sm text-slate-500">
-              <div className="text-sky-500">
-                Total Debts:
-                <span className="ml-1 font-semibold">
-                  {debtStats.total}
-                </span>
-              </div>
-              <div className="text-lime-500">
-                Paid:
-                <span className="ml-1 font-semibold">
-                  {debtStats.paid}
-                </span>
-              </div>
-              <div className="text-rose-500">
-                Unpaid:
-                <span className="ml-1 font-semibold">
-                  {debtStats.unpaid}
-                </span>
-              </div>
-            </div>
-          )}
+          {!isLoading && (<DebtStats debtStats={debtStats} />)}
         </div>
       </div>
     </div>
